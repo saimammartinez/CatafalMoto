970be701b1be8917dd2bc7dcdfd7c8d7cde7a415
Miky <mmartinez@saimasolutions.com>
2022-01-15 12:52:14 +0100

    Autenticación y Avatar

---
+++ catafal-moto/components/Auth.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { supabase } from '../utils/supabaseClient'
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleLogin = async (email) => {
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signIn({ email })
+      if (error) throw error
+      alert('Comprueba tu correo para obtener el enlace de login!')
+    } catch (error) {
+      alert(error.error_description || error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget">
+        <h1 className="header">Catafal Moto</h1>
+        <p className="description">Registrate vía magic link con tu email</p>
+        <div>
+          <input
+            className="inputField"
+            type="email"
+            placeholder="Tu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              handleLogin(email)
+            }}
+            className="button block"
+            disabled={loading}
+          >
+            <span>{loading ? 'Cargando' : 'Enviar magic link'}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
